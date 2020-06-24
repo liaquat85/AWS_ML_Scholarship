4413f9535453ccee396f0396755225bb7041e25d
liaquat85 <liaquat85@gmail.com>
2020-06-24 08:30:30 -0400

    lesson 5

---
+++ Lesson 4/uploading_distribution/gb_ali2_distributions/README.md
@@ -0,0 +1,7 @@
+#this is read file to give discription of the package
+
+Summary of the files
+
+#How to install
+
+What formulae I have used for distribution 
---
+++ Lesson 4/uploading_distribution/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gb_ali2_distributions',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['gb_ali2_distributions'],
+      autho='Liaquat Ali',
+      author_email = 'liaquat85@gmail.com',
+      zip_safe=False)
